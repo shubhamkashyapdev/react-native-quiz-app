abccc1b50a06dfebe21672a974c1aed8fd7e96fa
Shubham Kashyap <hex.media.1800@gmail.com>
2021-10-03 01:13:25 +0530

    react native setup and project start

---
+++ screens/Result.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {StyleSheet, View, Text, Button} from 'react-native';
+
+const Result = () => {
+  return <View style={styles.container}></View>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 40,
+    paddingBottom: 50,
+  },
+});
+
+export default Result;
